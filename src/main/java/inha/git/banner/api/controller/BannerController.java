3929909fba44043820d3c8227e5d450f5811eca1
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-08-12 22:15:14 +0900

    feat/#2 banner entity 생성

---
+++ src/main/java/inha/git/banner/api/controller/BannerController.java
@@ -0,0 +1,19 @@
+package inha.git.banner.api.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * BannerController는 유저 관련 엔드포인트를 처리.
+ */
+@Slf4j
+@Tag(name = "banner controller", description = "banner 관련 API")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/banner")
+public class BannerController {
+
+}
---
+++ src/main/java/inha/git/banner/domain/Banner.java
@@ -0,0 +1,38 @@
+package inha.git.banner.domain;
+
+import inha.git.common.BaseEntity;
+import inha.git.user.domain.User;
+import inha.git.user.domain.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+
+/**
+ * Banner 엔티티는 애플리케이션의 배너 정보를 나타냄.
+ */
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@Getter
+@Builder
+@Entity
+@Table(name = "banner_tb")
+public class Banner extends BaseEntity {
+
+    @Id
+    @Column(name = "banner_id", nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 50, name = "img_path")
+    private String imgPath;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
