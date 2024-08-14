144a192c9609d2490b5366916dda500d329f7372
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-08-15 00:09:27 +0900

    feat/#12: 공지사항 상세 조회 기능 추가

--- src/main/java/inha/git/notice/api/controller/dto/response/SearchNoticesResponse.java
+++ src/main/java/inha/git/notice/api/controller/dto/response/SearchNoticesResponse.java
@@ -1,12 +1,14 @@
 package inha.git.notice.api.controller.dto.response;
 
+import com.querydsl.core.annotations.QueryProjection;
+import inha.git.notice.domain.Notice;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDateTime;
 
-public record SearchNoticeResponse(
+public record SearchNoticesResponse(
         @NotNull
         @Schema(description = "공지사항 아이디", example = "1")
         Integer idx,
@@ -17,12 +19,18 @@ public record SearchNoticeResponse(
         String title,
 
         @NotNull
-        @Size(min = 1)
-        @Schema(description = "내용", example = "공지사항 내용")
-        String contents,
-        @NotNull
         @Schema(description = "작성일", example = "2021-08-01T00:00:00")
         LocalDateTime createdAt,
         @NotNull
         SearchNoticeUserResponse searchNoticeUserResponse
-) { }
+) {
+        @QueryProjection
+        public SearchNoticesResponse(Notice notice, SearchNoticeUserResponse searchNoticeUserResponse) {
+                this(
+                        notice.getId(),
+                        notice.getTitle(),
+                        notice.getCreatedAt(),
+                        searchNoticeUserResponse
+                );
+        }
+}
