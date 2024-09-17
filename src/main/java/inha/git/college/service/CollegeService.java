4e853b8efb8f84c27880b0049d57cd12854e92fd
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-09-17 17:50:28 +0900

    feat/#95: 단과대 관련 로그 추가

--- src/main/java/inha/git/college/service/CollegeService.java
+++ src/main/java/inha/git/college/service/CollegeService.java
@@ -3,6 +3,7 @@ package inha.git.college.service;
 import inha.git.college.controller.dto.request.CreateCollegeRequest;
 import inha.git.college.controller.dto.request.UpdateCollegeRequest;
 import inha.git.college.controller.dto.response.SearchCollegeResponse;
+import inha.git.user.domain.User;
 
 import java.util.List;
 
@@ -10,10 +11,10 @@ public interface CollegeService {
 
     List<SearchCollegeResponse> getColleges();
     SearchCollegeResponse getCollege(Integer departmentIdx);
-    String createCollege(CreateCollegeRequest createDepartmentRequest);
-    String updateCollegeName(Integer collegeIdx, UpdateCollegeRequest updateCollegeRequest);
+    String createCollege(User admin, CreateCollegeRequest createDepartmentRequest);
+    String updateCollegeName(User admin, Integer collegeIdx, UpdateCollegeRequest updateCollegeRequest);
 
-    String deleteCollege(Integer collegeIdx);
+    String deleteCollege(User admin, Integer collegeIdx);
 
 
 
