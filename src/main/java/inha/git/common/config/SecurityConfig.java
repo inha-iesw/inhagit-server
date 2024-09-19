9f9090f446d2f446178c00862f999cd65728941e
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-09-19 20:46:44 +0900

    fix/#104: 누락된 "Referrer policy" 보안 헤더 추가

--- src/main/java/inha/git/common/config/SecurityConfig.java
+++ src/main/java/inha/git/common/config/SecurityConfig.java
@@ -11,10 +11,12 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 
 import static inha.git.user.domain.enums.Permission.*;
 import static inha.git.user.domain.enums.Role.*;
@@ -92,7 +94,11 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-
+                .headers(headers -> headers
+                        .referrerPolicy(referrerPolicy -> referrerPolicy.policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.NO_REFERRER))
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::deny)
+                        .contentSecurityPolicy(contentSecurityPolicy -> contentSecurityPolicy.policyDirectives("default-src 'self'"))
+                )
                 .authorizeHttpRequests(req ->
 
                         req.requestMatchers(WHITE_LIST_URL).permitAll().
