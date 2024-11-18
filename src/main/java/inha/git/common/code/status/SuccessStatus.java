8a7e5bd8cac07be1c72211e8e3e814d25bea4b3b
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-11-18 18:35:21 +0900

    feat/#177: 마이페이지 버그 제보 전체 조회 API 추가

--- src/main/java/inha/git/common/code/status/SuccessStatus.java
+++ src/main/java/inha/git/common/code/status/SuccessStatus.java
@@ -175,6 +175,7 @@ public enum SuccessStatus implements BaseCode {
     MY_PAGE_PROBLEM_SEARCH_OK(HttpStatus.OK, "USER2004", "마이페이지 문제 조회 성공"),
     PW_CHANGE_OK(HttpStatus.OK, "USER2005", "비밀번호 변경 성공"),
     MY_PAGE_REPORT_SEARCH_OK(HttpStatus.OK, "USER2006", "마이페이지 신고 조회 성공"),
+    MY_PAGE_BUG_REPORT_SEARCH_OK(HttpStatus.OK, "USER2007", "마이페이지 버그 제보 조회 성공"),
 
     BUG_REPORT_CREATE_OK(HttpStatus.CREATED, "BUG_REPORT2010", "버그 제보 성공"),
     BUG_REPORT_UPDATE_OK(HttpStatus.OK, "BUG_REPORT2001", "버그 제보 수정 성공"),
