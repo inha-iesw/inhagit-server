581f48dfc3ac4a98c97746431fdebf3ba8eee94e
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2024-12-18 15:36:39 +0900

    feat/#197: 페이징 조회 시 사이즈 입력받도록 변경

--- src/main/java/inha/git/notice/api/service/NoticeService.java
+++ src/main/java/inha/git/notice/api/service/NoticeService.java
@@ -11,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 public interface NoticeService {
-    Page<SearchNoticesResponse> getNotices(Integer page);
+    Page<SearchNoticesResponse> getNotices(Integer page, Integer size);
     String createNotice(User user, CreateNoticeRequest createNoticeRequest, List<MultipartFile> attachmentList);
 
     String updateNotice(User user, Integer noticeIdx, UpdateNoticeRequest updateNoticeRequest, List<MultipartFile> attachmentList);
