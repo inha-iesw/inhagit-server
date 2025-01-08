0caeaa6444a72d105545940d5e75d3a6b78672e9
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-01-08 18:00:05 +0900

    feat/#234: 질문 댓글 관련 좋아요 비관적락 추가

--- src/main/java/inha/git/question/domain/repository/QuestionReplyCommentJpaRepository.java
+++ src/main/java/inha/git/question/domain/repository/QuestionReplyCommentJpaRepository.java
@@ -1,11 +1,15 @@
 package inha.git.question.domain.repository;
 
 
-import inha.git.common.BaseEntity;
 import inha.git.common.BaseEntity.State;
 import inha.git.question.domain.QuestionComment;
 import inha.git.question.domain.QuestionReplyComment;
+import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 
 import java.util.Optional;
 
@@ -17,6 +21,11 @@ public interface QuestionReplyCommentJpaRepository extends JpaRepository<Questio
 
 
     Optional<QuestionReplyComment> findByIdAndState(Integer commentIdx, State state);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value = "3000")})
+    @Query("SELECT c FROM QuestionReplyComment c WHERE c.id = :commentIdx AND c.state = :state")
+    Optional<QuestionReplyComment> findByIdAndStateWithPessimisticLock(Integer commentIdx, State state);
 
     boolean existsByQuestionCommentAndState(QuestionComment questionComment, State state);
 }
