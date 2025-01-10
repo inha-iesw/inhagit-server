c4c50b740e78e544be5be71743c04ab0b7096ff4
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-01-10 14:08:47 +0900

    fix/#238: 새로운 통계 테이블 추가

---
+++ src/main/java/inha/git/statistics/domain/repository/StatisticsJpaRepository.java
@@ -0,0 +1,23 @@
+package inha.git.statistics.domain.repository;
+
+import inha.git.statistics.domain.Statistics;
+import inha.git.statistics.domain.TotalDepartmentStatistics;
+import inha.git.statistics.domain.enums.StatisticsType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface StatisticsJpaRepository extends JpaRepository<Statistics, Long>{
+
+    List<Statistics> findByStatisticsTypeAndTargetId(StatisticsType statisticsType, Long targetId);
+
+    List<Statistics> findBySemesterIdAndStatisticsType(Long semesterId, StatisticsType statisticsType);
+
+    List<Statistics> findByFieldIdAndStatisticsType(Long fieldId, StatisticsType statisticsType);
+
+    List<Statistics> findByCategoryIdAndStatisticsType(Long categoryId, StatisticsType statisticsType);
+    List<Statistics> findByStatisticsType(StatisticsType statisticsType);
+    Optional<Statistics> findByStatisticsTypeAndTargetIdIsNull(StatisticsType statisticsType);
+}
