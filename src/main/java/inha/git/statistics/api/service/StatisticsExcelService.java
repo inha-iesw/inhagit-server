7f6f0c238c877136b15f5f011278e73313fdbbd2
Gyuhyeok99 <126947828+Gyuhyeok99@users.noreply.github.com>
2025-01-12 23:18:10 +0900

    fix/#240: 전체, 단과대, 학과별, 유저 엑셀 통계 추출 기능 추가

--- src/main/java/inha/git/statistics/api/service/StatisticsExcelService.java
+++ src/main/java/inha/git/statistics/api/service/StatisticsExcelService.java
@@ -1,10 +1,11 @@
 package inha.git.statistics.api.service;
 
+import inha.git.statistics.domain.enums.StatisticsType;
 import jakarta.servlet.http.HttpServletResponse;
 
 public interface StatisticsExcelService {
 
-    void exportToExcelFile(HttpServletResponse response);
+    void exportToExcelFile(HttpServletResponse response, StatisticsType statisticsType, Integer filterId);
 
 
 }
