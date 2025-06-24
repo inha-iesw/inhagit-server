cd2ee506b00e508eceac8b614a9e69fe549929d2
dayjiwon <jiwon6232@gmail.com>
2025-06-25 02:35:31 +0900

    기업 회원 I-FOSS 접근 허용

--- src/main/java/inha/git/common/Constant.java
+++ src/main/java/inha/git/common/Constant.java
@@ -121,6 +121,7 @@ public class Constant {
         return project.getIsPublic() ||
                 project.getUser().getId().equals(user.getId()) ||
                 user.getRole().equals(Role.ADMIN) ||
-                user.getRole().equals(Role.PROFESSOR);
+                user.getRole().equals(Role.PROFESSOR) ||
+                user.getRole().equals(Role.COMPANY);
     }
 }
